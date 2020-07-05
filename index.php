e9d9cd8b06668844e5416b289c70a7d8db5ed438
LK <lukasz.fialki@gmail.com>
2020-07-05 16:16:57 +0200

    Change something

--- index.php
+++ index.php
@@ -15,7 +15,7 @@
 <link href="https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,300;0,400;0,600;0,700;0,800;1,300;1,400;1,600;1,700;1,800&display=swap" rel="stylesheet"> 
 <script src="https://code.jquery.com/jquery-1.12.4.min.js"></script>
 <link rel="stylesheet" href="css/main.css">
-<title>Kontrola Grubego</title>
+<title>Kontrola Grubego!</title>
 </head>
 <body>
 
